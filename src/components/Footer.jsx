bf6ca299baf99606930fd5183bd093a70f778307
Kevin Zhang <kevinzhang@utexas.edu>
2022-06-20 13:00:53 -0500

    Figma footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ export default function Footer() {
                 <a target="_blank" rel="noopener noreferrer"  class="link link-hover" href="https://github.com/ulzhang/resume/blob/94971228fe43326db578b6a722cfb5eaa4c693dc/Kevin_Zhang_Resume_Q2_22.pdf">Resume</a> 
                 <a target="_blank" rel="noopener noreferrer" class="link link-hover" href="https://www.linkedin.com/in/kevinczhang/">Linkedin</a> 
                 <a target="_blank" rel="noopener noreferrer" class="link link-hover" href="https://github.com/ulzhang">Github</a> 
+                <a target="_blank" rel="noopener noreferrer" class="link link-hover" href="https://www.figma.com/file/6goyu9ADKdjU7I2vXa22pR/House-Zero-Listing">Figma</a>
             </div> 
             <div>
                 <p>House Zero Mock Listing - Kevin Zhang 2022</p>
